cbf7b396164e7a35c30850b36a89c4947563c6b0
Blaudio <bvargas_mont@hotmail.com>
2015-10-07 11:51:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "Hash_enca.h"
+#include "Hash_dir.h"
+//#define TAMANO 9
 
+int main(){
+	Clave *clave;
+//	Nodo *tabla[TAMANO];//hash_enca
+	Registro *tabla[TAMANO];//hash_dir
+	int k;
+	clave=inicializa_clave();
+	tabla[TAMANO]=inicializa_tabla();
+//	k=hash(clave);//hash_enca
+	k=hash(tabla[TAMANO],clave, 0);//hash_dir
+	printf("%i\n",k);
+	//llenar_tabla(tabla[TAMANO],clave,k);
+}
