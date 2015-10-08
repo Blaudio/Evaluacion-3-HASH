a906b68e4445ef51fbddf2a757a8d6a651dde801
Blaudio <bvargas_mont@hotmail.com>
2015-10-08 13:01:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "Hash_enca.h"
-#include "Hash_dir.h"
+#include "Hash_enca.h"
 
 int main(){
 	Clave *clave;
-//	Nodo *tabla[TAMANO];//hash_enca
-	Registro *tabla[TAMANO];//hash_dir
+	Nodo *tabla[TAMANO];//hash_enca
 	int k;//hash_enca
-	clave=inicializa_clave(6);
-	tabla[TAMANO]=inicializa_tabla();
+//	clave=inicializa_clave(6);
+//	tabla[TAMANO]=inicializa_tabla();
 //	k=hash(clave);//hash_enca
-	k=hash(tabla,clave, 0);//hash_dir
-//	tabla[k]->dato=clave;//hash_dir
-//	tabla[k]->dato->clave=clave;//hash_enca
-	printf("%i\n",k);
+//	tabla[k]=completar_tabla(tabla,clave);//hash_enca
+//	printf("%i\n",k);
 	//llenar_tabla(tabla[TAMANO],clave,k);
 }
