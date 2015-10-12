8212f2d048706fd024b891b33735450de77f71e8
Blaudio <bvargas_mont@hotmail.com>
2015-10-12 17:18:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include "Hash_enca.h"
+//#include "Archivos.h"
+#ifndef hash_h
 #include "Hash_enca.h"
+#define hash_h
+#endif
 
 int main(){
 	Clave *clave;
-	Nodo *tabla[TAMANO];//hash_enca
-	int k;//hash_enca
-//	clave=inicializa_clave(6);
-//	tabla[TAMANO]=inicializa_tabla();
-//	k=hash(clave);//hash_enca
-//	tabla[k]=completar_tabla(tabla,clave);//hash_enca
-//	printf("%i\n",k);
-	//llenar_tabla(tabla[TAMANO],clave,k);
+	Nodo *tabla[TAMANO];
+	*tabla=inicializa_tabla();
+	int aux_year, aux_conexiones, aux_conexiones2;
+	char aux_region[3], aux_month[2], basura;
+	char fila[80];
+	FILE* fichero;
+	fichero = fopen ("D:\\conexiones.txt", "r");
+	while (feof(fichero)==0){
+		fgets(fila, 80, (fichero));
+		fscanf(fichero,"%i	%s %s %d %c %d",&aux_year,&aux_region,&aux_month,&aux_conexiones,&basura,&aux_conexiones2);
+		aux_conexiones=aux_conexiones*1000+aux_conexiones2;
+		//printf("  conexiones    = %d  solo bromeo estoy aburrido\n\n", aux_conexiones);
+		clave=inicializa_clave(aux_year, aux_region, aux_month, aux_conexiones);
+		*tabla=completar_tabla_encadenada(*tabla, clave, aux_conexiones);
+		//printf("  conexiones    = %d  solo bromeo estoy aburrido\n\n", clave->conexiones);
+		getchar();
+		//*tabla[]=completar_tabla_encadenada(*tabla, clave, aux_conexiones);
+		//*tabla=completar_tabla_encadenada(*tabla, clave, aux_conexiones);
+		printf("  conexiones    = %d  solo bromeo estoy aburrido\n\n", tabla[1]->dato->clave->identificador);
+		}
+	fclose(fichero);
 }
