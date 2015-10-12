8877b4f0a5b9f3405d8ce3c54d419141084c808a
Blaudio <bvargas_mont@hotmail.com>
2015-10-12 17:20:38 -0300

    Update Hash_enca.h

--- Hash_enca.h
+++ Hash_enca.h
@@ -1,85 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAMANO 501
-
-//typedef struct _clave{
-//	int año;
-//	char region;
-//	char mes;
-//	int conexiones;
-//}Clave;
-
-//typedef struct _clave{
-//	int identificador;
-//}Clave;
+#define TAMANO 11
 
 typedef struct _clave{
 	int year;
-	char region;
-	char month;
+	char region[3];
+	char month[2];
 	int conexiones;
 	int identificador;
 }Clave;
 
-//typedef struct _registro{
-//	Clave *clave;
-//}Registro;
 
 typedef struct _registro{
 	Clave *clave;
-	struct Registro *sgt_dato
+	struct Registro *sgt_dato;
 }Registro;
 
 typedef struct _nodo{
 	Registro *dato;
-	//struct Nodo *sgt_dato;
 }Nodo;
 
 Nodo *tabla[TAMANO];
 
-Clave *inicializa_clave(int year, char region, char month, int conexiones){
+Clave *inicializa_clave(int year, char region[3], char month[2], int conexiones){
 	Clave *tmp;
 	tmp=malloc(sizeof(Clave));
 	tmp->year=year;
-	tmp->region=region;
-	tmp->month=month;
+	tmp->region[3]=region[3];
+	tmp->month[2]=month[2];
 	tmp->conexiones=conexiones;
 	tmp->identificador=hash(conexiones);
 	return(tmp);
 }
 
-//int año, char region, char mes, int conexiones
-
-//Nodo *inicializa_tabla(){
-//	Nodo *aux[TAMANO];
-//	aux[TAMANO]=malloc(sizeof(Nodo));
-//	aux[TAMANO]->dato=NULL;
-//	aux[TAMANO]->sgt_dato=NULL;
-//	return(aux[TAMANO]);
-//}
-
 Nodo *inicializa_tabla(){
 	Nodo *aux[TAMANO];
-	aux[TAMANO]=malloc(sizeof(Nodo));
-	aux[TAMANO]->datos=NULL;
-//	aux[TAMANO]->sgt_dato=NULL;
-	return(aux[TAMANO]);
+	int contador;
+	for(contador=0; contador<TAMANO; contador++){
+		aux[contador]=malloc(sizeof(Nodo));
+		aux[contador]->dato=NULL;
+		printf(" tabla completada %d  \n", contador);
+	}
+	return(*aux);
 }
 
-//Nodo *completar_tabla(Nodo *aux[TAMANO], Clave *tmp){
-//	int k;//realizado;
-//	k=tmp->identificador;
-//	realizado=0;
-//	if(aux[k]->dato==NULL){
-//		aux[k]->dato->clave=tmp;
-//		return(aux[TAMANO]);
-//	}else{
-//		//return(completar_tabla(aux[k]->sgt_dato,tmp))
-//	}
-//}
 
 Nodo *completar_tabla_encadenada(Nodo *aux[TAMANO], Clave *tmp, int k){
-	
+	getchar();
+	aux[k]->dato->clave=tmp;
+	getchar();
+	return(aux[k]);
 }
 
 int hash(int conexiones){
