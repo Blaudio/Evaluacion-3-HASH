96d96b98facddbfbdda4020470acd9c42ff5bd15
Blaudio <bvargas_mont@hotmail.com>
2015-10-13 17:10:37 -0300

    Update Hash_enca.h

--- Hash_enca.h
+++ Hash_enca.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAMANO 11
+#define TAMANO 501
 
 typedef struct _clave{
 	int year;
@@ -10,26 +10,27 @@ typedef struct _clave{
 	int identificador;
 }Clave;
 
-
-typedef struct _registro{
-	Clave *clave;
-	struct Registro *sgt_dato;
-}Registro;
-
 typedef struct _nodo{
-	Registro *dato;
+	Clave *clave;
+	struct Nodo *sgt_dato;
 }Nodo;
 
 Nodo *tabla[TAMANO];
 
-Clave *inicializa_clave(int year, char region[3], char month[2], int conexiones){
+
+Clave *inicializa_clave(int year, char region[3], char month[2], int conexiones, int aux_hash){
 	Clave *tmp;
 	tmp=malloc(sizeof(Clave));
 	tmp->year=year;
+	tmp->region[0]=region[0];
+	tmp->region[1]=region[1];
+	tmp->region[2]=region[2];
 	tmp->region[3]=region[3];
+	tmp->month[0]=month[0];
+	tmp->month[1]=month[1];
 	tmp->month[2]=month[2];
 	tmp->conexiones=conexiones;
-	tmp->identificador=hash(conexiones);
+	tmp->identificador=aux_hash;
 	return(tmp);
 }
 
@@ -38,18 +39,20 @@ Nodo *inicializa_tabla(){
 	int contador;
 	for(contador=0; contador<TAMANO; contador++){
 		aux[contador]=malloc(sizeof(Nodo));
-		aux[contador]->dato=NULL;
-		printf(" tabla completada %d  \n", contador);
+		aux[contador]->clave=NULL;
+		aux[contador]->sgt_dato=NULL;
 	}
 	return(*aux);
 }
 
 
-Nodo *completar_tabla_encadenada(Nodo *aux[TAMANO], Clave *tmp, int k){
-	getchar();
-	aux[k]->dato->clave=tmp;
-	getchar();
-	return(aux[k]);
+Nodo *completar_tabla_encadenada(Nodo *aux, Clave *tmp, int k){
+	if(aux->clave!=NULL){
+		aux->clave=tmp;
+		return(aux);
+	}else{
+		return(aux->sgt_dato=completar_tabla_encadenada(aux->sgt_dato,tmp,k));
+	}
 }
 
 int hash(int conexiones){
